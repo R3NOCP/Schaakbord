a3a1e3698cda4076306460633230585fc76271c9
LOGXSTAR <reno@logxstar.com>
2018-05-11 15:23:15 +0200

    [W2-JAVA-001]
    Schaakbord 8x8

---
+++ W2-JAVA-001.js
@@ -0,0 +1,11 @@
+// Schaakbord 8x8
+
+function chessBoard(size) {
+  for(var i = 0; i < size; i++) {
+      if (i => 0 === 0) {
+         console.log('' + '# # # # # # # #');
+       }
+    }
+ }
+
+chessBoard(8);
